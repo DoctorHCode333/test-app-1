23271fafec571d31a8d3a1a7a113e018ee2ad35c
DoctorHCode333 <harshvardhanbawake@gmail.com>
2024-06-03 18:08:31 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([])
+  useEffect (() =>{
+    fetchData();
+  },[])
+  
+  const fetchData = async () => {
+    await fetch('https://jsonplaceholder.typeicode.com/users')
+    .then((res) => res.json)
+    .then((data) => setUsers(data))
+    .catch((err) => {
+        console.log(err);
+    })
+  }
+
+  console.log(users)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <h3> React Crud Using Jsonplaceholder</h3>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
