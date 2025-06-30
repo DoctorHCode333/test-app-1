770daee7028721ee7d768b13a692dd6b18c90eee
HARSHVARDHAN BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2025-06-29 20:20:57 -0400

    Update Sync.py

--- src/Sync.py
+++ src/Sync.py
@@ -1,21 +1,23 @@
-
 import oracledb
 import pandas as pd
-from wordcloud import WordCloud
+from wordcloud import WordCloud,STOPWORDS
 import matplotlib.pyplot as plt
+import re
+from collections import Counter
 
 # Step 1: Connect to Oracle DB (thin mode)
+
+  #DB Detailshere ignore this
 connection = oracledb.connect(
-    user="USERNAME",
-    password="PASSWORD",
-    dsn="HOST:PORT/SERVICE_NAME"
+    #DB Detailshere ignore this
 )
 
 # Step 2: Query the database
 query = """
-    SELECT column_with_text
-    FROM your_table
-    WHERE ROWNUM <= 1000
+    SELECT PHRASE
+    FROM PSENTIMENTPHRASES
+    WHERE STARTDATE >TO_TIMESTAMP('2025-06-01 12:00:00 AM', 'YYYY-MM-DD HH:MI:SS AM') AND 
+STARTDATE < TO_TIMESTAMP('2025-06-05 12:00:00 AM', 'YYYY-MM-DD HH:MI:SS AM')
 """
 df = pd.read_sql(query, con=connection)
 
@@ -23,14 +25,48 @@ df = pd.read_sql(query, con=connection)
 connection.close()
 
 # Step 4: Combine all text into one string
-text_data = " ".join(df['column_with_text'].dropna().astype(str))
+text_data = " ".join(df['PHRASE'].dropna().astype(str))
 
+text_data = text_data.lower()
+
+text_data = re.sub(r'[^a-z\s]','',text_data)
+
+words =text_data.split()
+
+stopWords = set(STOPWORDS)
+
+customStopWords = ['something','oh','go','anything','know','you','just',
+                   'really','like','done','another','keep','from','also',
+                   'okay','want','could','would','customer','call','recording',
+                   'say','said','thing','get','got','make','dont','thats','well',
+                   'will','going','one','two','three','four','five','six','seven'
+                   'weve','yeah','didnt','mean','gonna','somebody','getting','saying',
+                   'telling','calling','shouldnt','theyre','called','already'
+                   'theres','havent','supposed','doesnt', 'uh', 'um', 'hmm', 'ah',
+                   'uhhuh', 'huh', 'alright', 'right', 'yeah', 'yep', 'nope', 'hey',
+                   'hi', 'hello', 'bye', 'goodbye', 'thanks', 'thank', 'please',
+                   'sure', 'fine', 'maybe', 'sorta', 'kinda', 'actually', 'basically',
+                   'literally', 'honestly', 'perhaps', 'probably', 'anyway', 'anyways',
+                   'well', 'happened', 'thing', 'stuff', 'lot', 'lots', 'bit', 'little',
+                   'ok', 'cool', 'oops', 'ugh', 'uhoh', 'yup', 'nah', 'dunno', 'lemme',
+                   'gimme', 'wanna', 'gotta', 'aint', 'isnt', 'wasnt', 'werent','someone',
+                   'hasnt', 'havent', 'hadnt', 'wont', 'wouldnt', 'couldnt', 'shouldnt', 'might',
+                   'mightve', 'must', 'mustve', 'seems', 'seemed', 'looks', 'looked', 'sounds',
+                   'sounded', 'feels', 'felt', 'guess', 'thinking', 'thought', 'believe', 'hope',
+                   'wish', 'try', 'tried', 'trying', 'see', 'seen', 'watch', 'watched', 'hear',
+                   'heard', 'listen', 'listened', 'talk', 'talked', 'talking', 'speak', 'spoke',
+                   'speaking', 'say', 'said', 'telling', 'told', 'ask', 'asked', 'asking'
+                   'everything','gotten','almost','sitting'
+]
+
+stopWords.update(customStopWords)
+
+filterWords = [word for word in words if word not in stopWords and len(word)>5]
+
+word_freq = Counter(filterWords)
+
+refinedText = " ".join(filterWords)[:500_000]
+
+#print(refinedText)
 # Step 5: Generate the word cloud
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text_data)
-
-# Step 6: Display the word cloud
-plt.figure(figsize=(15, 7))
-plt.imshow(wordcloud, interpolation='bilinear')
-plt.axis('off')
-plt.title("Word Cloud from Oracle DB Text Data")
-plt.show()
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_freq)
