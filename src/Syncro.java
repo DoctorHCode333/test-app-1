ba105aadf7678766f03b0dfb4abc52e0df95ddf3
HARSHVARDHAN BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2025-07-09 16:13:25 +0530

    Update Syncro.java

--- src/Syncro.java
+++ src/Syncro.java
@@ -102,3 +102,15 @@ public class ExcelWriter {
         }
     }
 }
+
+<!-- Required by Apache POI for logging -->
+<dependency>
+    <groupId>org.apache.logging.log4j</groupId>
+    <artifactId>log4j-api</artifactId>
+    <version>2.17.2</version>
+</dependency>
+<dependency>
+    <groupId>org.apache.logging.log4j</groupId>
+    <artifactId>log4j-core</artifactId>
+    <version>2.17.2</version>
+</dependency>
