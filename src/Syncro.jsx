62b4cccb234b0b298525b81ea1b6bf76c03d35a5
HARSHVARDHAN BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2025-07-24 21:13:16 +0530

    Update Syncro.jsx

--- src/Syncro.jsx
+++ src/Syncro.jsx
@@ -1,6 +1,5 @@
-$ /GenApps/PYTHON_APPLICATIONS/WordCloudServer/wordCloudServerVenv/bin/gunicorn wordCloudServer:app \ --certfile=/GenApp
-s/Certs/certificate \ --keyfile=/GenApps/Certs/certificate_key \ -w 4 \ -b 0.0.0.0:5000
-[2025-07-24 11:37:14 -0400] [2741956] [INFO] Starting gunicorn 23.0.0
-[2025-07-24 11:37:14 -0400] [2741956] [INFO] Listening at: http://127.0.0.1:8000 (2741956)
-[2025-07-24 11:37:14 -0400] [2741956] [INFO] Using worker: sync
-[2025-07-24 11:37:14 -0400] [2741957] [INFO] Booting worker with pid: 2741957
+/GenApps/PYTHON_APPLICATIONS/WordCloudServer/wordCloudServerVenv/bin/gunicorn wordCloudServer:app \
+  --certfile=/GenApps/Certs/certificate \
+  --keyfile=/GenApps/Certs/certificate_key \
+  -w 4 \
+  -b 0.0.0.0:5000
