e505530c42e70554eb2da30a68d90610164492f7
HARSHVARDHAN BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2025-07-31 18:03:40 +0530

    Update Syncro.java

--- src/Syncro.java
+++ src/Syncro.java
@@ -47,3 +47,57 @@ FROM (
 ) 
 GROUP BY topicname, lob
 ORDER BY topicname, lob;
+
+
+SELECT topicname,
+     COUNT(*) AS total_count,
+     COUNT(topicname),
+     ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (PARTITION BY lob), 2) AS percentage_in_lob,
+     lob,
+     XMLSERIALIZE(CONTENT 
+         XMLAGG(XMLELEMENT(e, all_phrases) ORDER BY all_phrases)
+     ) AS phrases 
+FROM (
+    SELECT CONVERSATION_ID, 
+           topicname, 
+           MAX(lob) as lob,
+           XMLSERIALIZE(CONTENT 
+               XMLAGG(XMLELEMENT(e, TOPICPHRASE) ORDER BY TOPICPHRASE)
+           ) AS all_phrases
+    FROM HIST_TOPICS_IXNS 
+    WHERE TRUNC(startdate) BETWEEN TO_DATE(:fromDate, 'YYYY-MM-DD') AND TO_DATE(:toDate, 'YYYY-MM-DD')
+    AND topicname NOT LIKE 'PAL%' 
+    AND topicname NOT LIKE 'Chat%' 
+    GROUP BY CONVERSATION_ID, topicname
+) 
+GROUP BY topicname, lob
+ORDER BY topicname, lob;
+
+
+SELECT topicname,
+     COUNT(*) AS total_count,
+     COUNT(topicname),
+     ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (PARTITION BY lob), 2) AS percentage_in_lob,
+     lob,
+     REGEXP_REPLACE(
+         XMLAGG(XMLELEMENT(e, all_phrases || '@@@') ORDER BY all_phrases).getClobVal(),
+         '@@@$', 
+         ''
+     ) AS phrases 
+FROM (
+    SELECT CONVERSATION_ID, 
+           topicname, 
+           MAX(lob) as lob,
+           REGEXP_REPLACE(
+               XMLAGG(XMLELEMENT(e, TOPICPHRASE || '@@@') ORDER BY TOPICPHRASE).getClobVal(),
+               '@@@$', 
+               ''
+           ) AS all_phrases
+    FROM HIST_TOPICS_IXNS 
+    WHERE TRUNC(startdate) BETWEEN TO_DATE(:fromDate, 'YYYY-MM-DD') AND TO_DATE(:toDate, 'YYYY-MM-DD')
+    AND topicname NOT LIKE 'PAL%' 
+    AND topicname NOT LIKE 'Chat%' 
+    GROUP BY CONVERSATION_ID, topicname
+) 
+GROUP BY topicname, lob
+ORDER BY topicname, lob;
