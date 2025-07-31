f9b8e38e238c9aeef5e08fa78653d4cecd2c7ed5
HARSHVARDHAN BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2025-07-31 18:20:15 +0530

    Update Syncro.java

--- src/Syncro.java
+++ src/Syncro.java
@@ -1,19 +1,18 @@
-SELECT topicname,
+      select topicname,
      COUNT(*) AS total_count,
      count(topicname),
      ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (PARTITION BY lob), 2) AS percentage_in_lob,
-     lob,
-     REGEXP_REPLACE(XMLCAST(XMLAGG(XMLELEMENT(e, ALL_PHRASES || '@@@') ORDER BY ALL_PHRASES) AS CLOB), '@@@$', '') AS phrases 
-FROM (
-    SELECT CONVERSATION_ID, topicname, Max(lob) as lob,
-           REGEXP_REPLACE(XMLCAST(XMLAGG(XMLELEMENT(e, TOPICPHRASE || '@@@') ORDER BY TOPICPHRASE) AS CLOB), '@@@$', '') AS all_phrases
-    FROM HIST_TOPICS_IXNS 
-    WHERE TRUNC(startdate) BETWEEN TO_DATE(:fromDate, 'YYYY-MM-DD') AND TO_DATE(:toDate, 'YYYY-MM-DD')
-    AND topicname NOT LIKE 'PAL%' AND topicname NOT LIKE 'Chat%' 
-    GROUP BY CONVERSATION_ID, topicname
-) 
-GROUP BY topicname, lob
-ORDER BY topicname, lob;
+     lob,RTRIM(XMLCAST(XMLAGG(XMLELEMENT(e, ALL_PHRASES) ORDER BY ALL_PHRASES) AS CLOB) || '@@@', '@@@') AS phrases 
+     FROM (
+        select CONVERSATION_ID, topicname, 
+        Max(lob) as lob,
+        RTRIM(XMLCAST(XMLAGG(XMLELEMENT(e, TOPICPHRASE || '@@@') ORDER BY TOPICPHRASE) AS CLOB), '@@@') AS all_phrases
+        FROM HIST_TOPICS_IXNS 
+        WHERE TRUNC(startdate) BETWEEN TO_DATE(:fromDate, 'YYYY-MM-DD') AND TO_DATE(:toDate, 'YYYY-MM-DD'
+        )
+    and topicname not like 'PAL%' and topicname not like 'Chat%' GROUP BY CONVERSATION_ID,topicname)  
+    GROUP BY topicname,lob
+    ORDER BY topicname,lob;
 
 
                                   
