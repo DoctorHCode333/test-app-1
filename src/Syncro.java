12b8c19b8869d8908362dec3906b6f3a5df7cdf4
HARSHVARDHAN BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2025-07-31 18:13:27 +0530

    Update Syncro.java

--- src/Syncro.java
+++ src/Syncro.java
@@ -101,3 +101,35 @@ FROM (
 ) 
 GROUP BY topicname, lob
 ORDER BY topicname, lob;
+
+SELECT topicname,
+     COUNT(*) AS total_count,
+     COUNT(topicname),
+     ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (PARTITION BY lob), 2) AS percentage_in_lob,
+     lob,
+     RTRIM(
+         XMLCAST(
+             XMLAGG(XMLELEMENT(e, all_phrases) ORDER BY all_phrases) 
+             AS CLOB
+         ) || '@@@', 
+         '@@@'
+     ) AS phrases 
+FROM (
+    SELECT CONVERSATION_ID, 
+           topicname, 
+           MAX(lob) as lob,
+           RTRIM(
+               XMLCAST(
+                   XMLAGG(XMLELEMENT(e, TOPICPHRASE) ORDER BY TOPICPHRASE) 
+                   AS CLOB
+               ) || '@@@', 
+               '@@@'
+           ) AS all_phrases
+    FROM HIST_TOPICS_IXNS 
+    WHERE TRUNC(startdate) BETWEEN TO_DATE(:fromDate, 'YYYY-MM-DD') AND TO_DATE(:toDate, 'YYYY-MM-DD')
+    AND topicname NOT LIKE 'PAL%' 
+    AND topicname NOT LIKE 'Chat%' 
+    GROUP BY CONVERSATION_ID, topicname
+) 
+GROUP BY topicname, lob
+ORDER BY topicname, lob;
