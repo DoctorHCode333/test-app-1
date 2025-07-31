9c1f00a5217fd1b23453950f3359122f7cd5bc0f
HARSHVARDHAN BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2025-07-31 17:57:09 +0530

    Update Syncro.java

--- src/Syncro.java
+++ src/Syncro.java
@@ -3,10 +3,10 @@ SELECT topicname,
      count(topicname),
      ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (PARTITION BY lob), 2) AS percentage_in_lob,
      lob,
-     LISTAGG(ALL_PHRASES, '@@@') WITHIN GROUP (ORDER BY ALL_PHRASES) AS phrases 
+     REGEXP_REPLACE(XMLCAST(XMLAGG(XMLELEMENT(e, ALL_PHRASES || '@@@') ORDER BY ALL_PHRASES) AS CLOB), '@@@$', '') AS phrases 
 FROM (
     SELECT CONVERSATION_ID, topicname, Max(lob) as lob,
-           LISTAGG(TOPICPHRASE, '@@@') WITHIN GROUP (ORDER BY TOPICPHRASE) AS all_phrases
+           REGEXP_REPLACE(XMLCAST(XMLAGG(XMLELEMENT(e, TOPICPHRASE || '@@@') ORDER BY TOPICPHRASE) AS CLOB), '@@@$', '') AS all_phrases
     FROM HIST_TOPICS_IXNS 
     WHERE TRUNC(startdate) BETWEEN TO_DATE(:fromDate, 'YYYY-MM-DD') AND TO_DATE(:toDate, 'YYYY-MM-DD')
     AND topicname NOT LIKE 'PAL%' AND topicname NOT LIKE 'Chat%' 
