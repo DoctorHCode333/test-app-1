394782cf9ab1ab7427a9bcadc97de40855c10bf6
HARSHVARDHAN BAWAKE <94169362+DoctorHCode333@users.noreply.github.com>
2025-07-31 17:52:35 +0530

    Update Syncro.java

--- src/Syncro.java
+++ src/Syncro.java
@@ -1,9 +1,16 @@
- select topicname,
+SELECT topicname,
      COUNT(*) AS total_count,
      count(topicname),
      ROUND((COUNT(*) * 100.0) / SUM(COUNT(*)) OVER (PARTITION BY lob), 2) AS percentage_in_lob,
-     lob,RTRIM(XMLCAST(XMLAGG(XMLELEMENT(e, ALL_PHRASES || '@@@') ORDER BY ALL_PHRASES) AS CLOB), '@@@') AS phrases FROM (select CONVERSATION_ID, topicname, Max(lob) as lob,RTRIM(XMLCAST(XMLAGG(XMLELEMENT(e, TOPICPHRASE || '@@@') ORDER BY TOPICPHRASE) AS CLOB), '@@@') AS all_phrases
+     lob,
+     LISTAGG(ALL_PHRASES, '@@@') WITHIN GROUP (ORDER BY ALL_PHRASES) AS phrases 
+FROM (
+    SELECT CONVERSATION_ID, topicname, Max(lob) as lob,
+           LISTAGG(TOPICPHRASE, '@@@') WITHIN GROUP (ORDER BY TOPICPHRASE) AS all_phrases
     FROM HIST_TOPICS_IXNS 
     WHERE TRUNC(startdate) BETWEEN TO_DATE(:fromDate, 'YYYY-MM-DD') AND TO_DATE(:toDate, 'YYYY-MM-DD')
-    and topicname not like 'PAL%' and topicname not like 'Chat%' GROUP BY CONVERSATION_ID,topicname)  GROUP BY topicname,lob
-    ORDER BY topicname,lob;
+    AND topicname NOT LIKE 'PAL%' AND topicname NOT LIKE 'Chat%' 
+    GROUP BY CONVERSATION_ID, topicname
+) 
+GROUP BY topicname, lob
+ORDER BY topicname, lob;
